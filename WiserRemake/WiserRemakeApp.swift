fd286703684f3c79e3aa685a492a0b842f7ac74b
Adrien Surugue <adrien.surugue@gmail.com>
2021-09-09 14:28:03 +0200

    Initial Commit

---
+++ WiserRemake/WiserRemakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiserRemakeApp.swift
+//  WiserRemake
+//
+//  Created by Adrien Surugue on 09/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct WiserRemakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
